5b2e56e98645fad3851111215382bb3a63c779e0
CHOPSUEY10 <fadliaidin16@gmail.com>
2026-06-05 20:43:59 +0700

    feat: add global layout with ToastContainer and stylesheet integration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import ToastContainer from '$lib/components/ToastContainer.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <ToastContainer />
 {@render children()}
