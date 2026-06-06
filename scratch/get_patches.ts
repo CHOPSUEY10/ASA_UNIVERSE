dcacd65b8402c8a4ab89da3499eb40e85889d91b
CHOPSUEY10 <fadliaidin16@gmail.com>
2026-06-06 18:00:45 +0700

    refactor: remove deprecated utility functions and unused helper modules

--- scratch/get_patches.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from '../src/lib/server/prisma';
-
-async function main() {
-    const patches = await prisma.patch.findMany();
-    console.log(patches);
-}
-
-main().finally(() => prisma.$disconnect());
